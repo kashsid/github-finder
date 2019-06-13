c679a04d13f022dd8ad77a2a07d8284f3566b079
kashsid <kashifsiddique@gmail.com>
2019-06-13 14:50:43 -0500

    implements component state on user Item component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component} from "react";
-import Navbar from './components/layout/Navbar'
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Useritem from "./components/user/Useritem";
 import "./App.css";
 
 class App extends Component {
-render(){
-
-  return (
-    <div className="App">
-     <Navbar/>
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Useritem/>
+      </div>
+    );
+  }
 }
 export default App;
