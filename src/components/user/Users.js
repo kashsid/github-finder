f2e9b8d0491a8a64186f6b3825af678eec33e62b
kashsid <kashifsiddique@gmail.com>
2019-06-14 09:39:02 -0500

    fetches data from github usr api using axios

--- src/components/user/Users.js
+++ src/components/user/Users.js
@@ -3,32 +3,11 @@ import Useritem from './Useritem';
 
 
 export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 1,
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-      },
-      {
-        id: 2,
-        login: "defunkt",
-        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt"
-      },
-      {
-        id: 3,
-        login: "pjhyett",
-        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/pjhyett"
-      }
-    ]
-  };
+  
   render() {
     return (
       <div className="grid-3">
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <Useritem key={user.id} user={user}/>
         ))}
       </div>
